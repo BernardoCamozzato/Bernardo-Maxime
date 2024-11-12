cb3847f5eaec97e94612d4e3c32cbe473cc83d55
Maxime Awesso <maximeawesso8@gmail.com>
2024-11-12 16:07:29 +0100

    Creer une nouvelle fenêtre

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 from pygame.locals import *
 
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 380))
+
+continuer = True
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+
+pygame.quit()
+
+
