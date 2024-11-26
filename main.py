91ba0e653625ee65e8d8db78683efdae45948225
Maxime Awesso <maximeawesso8@gmail.com>
2024-11-26 16:35:44 +0100

    classe obstacles

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
+from pygame.examples.sprite_texture import sprite
 from pygame.locals import *
 
 pygame.init()
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,x,y, point_de_vie):
+        super().__init__()
+        self.point_de_vie= point_de_vie
+        self.position_x= x
+        self.position_y=y
+
+    def casser(self, point_de_vie):
+        self.point_de_vie=point_de_vie
+        if self.point_de_vie<=0:
+            sprite.kill()
+
+class Bonus(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
 
 fenetre = pygame.display.set_mode((400, 600))
 
@@ -48,6 +66,7 @@ while continuer:
                 personage_1.rect = personage_1.rect.move(10,0)
 
 
+
+
+
 pygame.quit()
-
-
